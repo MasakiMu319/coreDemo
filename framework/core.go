e6ab29035ccdabe937bcf09436fdc898032a412b
麋鹿先生 <alulu@miluxiaengdeMBP.mshome.net>
2021-10-15 16:42:51 +0800

    add framework-context and do test

--- framework/core.go
+++ framework/core.go
@@ -1,15 +1,32 @@
 package framework
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 type Core struct {
-
+	router map[string]ControllerHandler
 }
 
 func NewCore() *Core {
-	return &Core{}
+	return &Core{router: map[string]ControllerHandler{}}
+}
+
+func (c *Core) Get(url string, handler ControllerHandler) {
+	c.router[url] = handler
 }
 
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	// TODO
+	log.Println("core.serveHTTP")
+	ctx := NewContext(request, response)
+
+	// 只是方便测试，写死为 foo
+	router := c.router["foo"]
+	if router == nil {
+		return
+	}
+	log.Println("core.router")
+
+	router(ctx)
 }
--- route.go
+++ route.go
@@ -1,5 +1,7 @@
 package main
 
-func routeHandler()  {
-	// TODO
+import "coreDemo/framework"
+
+func registerRouter(core *framework.Core) {
+	core.Get("foo", FooControllerHandler)
 }
